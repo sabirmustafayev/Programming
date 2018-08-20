ea57ad1f10172a270409e8d9789f4c10e85cd055
Sabir Mustafayev <sabirmustafayev86@gmail.com>
2018-08-20 02:02:55 -0400

    Print "Sabir Mustafayev" on Console Screen
    Print "Sabir Mustafayev" on Console Screen

--- FirstConsoleApp.cs
+++ FirstConsoleApp.cs
@@ -13,7 +13,7 @@ namespace First_Console_App
             string Name = "Sabir";
             string LastName = "Mustafayev";
             Console.Write(Name);
-            Console.WriteLine(LastName);
+            Console.Write(LastName);
             Console.ReadKey();
         }
     }
